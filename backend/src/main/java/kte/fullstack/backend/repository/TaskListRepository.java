39026a56010fd7fc049a7cf70b65c8a48bcaf4a0
ledemkam <ledemkam@gmail.com>
2024-11-16 13:24:38 +0100

    self introduce method repository

--- backend/src/main/java/kte/fullstack/backend/repository/TaskListRepository.java
+++ backend/src/main/java/kte/fullstack/backend/repository/TaskListRepository.java
@@ -2,8 +2,11 @@ package kte.fullstack.backend.repository;
 
 import kte.fullstack.backend.entity.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+
 }
--- backend/src/main/java/kte/fullstack/backend/repository/TaskRepository.java
+++ backend/src/main/java/kte/fullstack/backend/repository/TaskRepository.java
@@ -3,7 +3,11 @@ package kte.fullstack.backend.repository;
 import kte.fullstack.backend.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByTaskListId(UUID taskListId);
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID Id);
 }
